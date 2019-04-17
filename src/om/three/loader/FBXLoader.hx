a72b9b662cfbcbeb2bc6bcd02b9ed65a08207084
tong <tong@disktree.net>
2019-04-17 14:19:08 +0200

    Fix

--- src/om/three/loader/FBXLoader.hx
+++ src/om/three/loader/FBXLoader.hx
@@ -1,16 +1,17 @@
 package om.three.loader;
 
 import three.core.Geometry;
+import three.loaders.LoadingManager;
 import three.materials.Material;
 
 @:native("THREE.FBXLoader")
 extern class FBXLoader {
-
 	var withCredentials : Bool;
-
-	function new( ?manager : three.loaders.LoadingManager ) : Void;
-
+	function new( ?manager : LoadingManager ) : Void;
 	function load( url : String, onLoad : Dynamic->Void, ?onProgress : Dynamic->Void, ?onError: Dynamic->Void ) : Void;
-	function setTexturePath( value : String ) : Void;
+	function setPath( path : String ) : FBXLoader;
+	function setResourcePath( value : String ) : FBXLoader;
+	function setCrossOrigin( value : String ) : FBXLoader;
 	function parse( json : Dynamic, ?texturePath : String ) : { geometry: Geometry, materials: Array<Material> };
+	//function setTexturePath( value : String ) : Void;
 }
--- src/om/three/loader/GLTFLoader.hx
+++ src/om/three/loader/GLTFLoader.hx
@@ -1,13 +1,15 @@
 package om.three.loader;
 
+import three.loaders.LoadingManager;
+
 @:native("THREE.GLTFLoader")
 extern class GLTFLoader {
 	var crossOrigin(default,null) : String;
-	function new() : Void;
+	function new( ?manager : LoadingManager ) : Void;
 	function load( url : String, ?onLoad : Dynamic->Void, ?onProgress : Dynamic->Void, ?onError : Dynamic->Void ) : Void;
 	function setCrossOrigin( value : String ) : GLTFLoader;
-	function setPath( value : String ) : GLTFLoader;
+	function setPath( path : String ) : GLTFLoader;
 	function setResourcePath( value : String ) : GLTFLoader;
 	function setDRACOLoader( dracoLoader : String ) : GLTFLoader;
-	//function parse( data : Dynamic, path : String, ?onLoad : Dynamic->Void, ?onError : Dynamic->Void ) : Void;
+	function parse( data : Dynamic, path : String, ?onLoad : Dynamic->Void, ?onError : Dynamic->Void ) : Void;
 }
