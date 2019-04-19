ead07fbd17d6c5bbecd338eadc8145206e4fb476
tong <tong@disktree.net>
2019-04-19 10:39:13 +0200

    Improve loaders

--- src/om/three/loader/FBXLoader.hx
+++ src/om/three/loader/FBXLoader.hx
@@ -1,14 +1,17 @@
 package om.three.loader;
 
+import js.html.ErrorEvent;
+import js.html.ProgressEvent;
 import three.core.Geometry;
 import three.loaders.LoadingManager;
 import three.materials.Material;
+import three.objects.Group;
 
 @:native("THREE.FBXLoader")
 extern class FBXLoader {
 	var withCredentials : Bool;
 	function new( ?manager : LoadingManager ) : Void;
-	function load( url : String, onLoad : Dynamic->Void, ?onProgress : Dynamic->Void, ?onError: Dynamic->Void ) : Void;
+	function load( url : String, onLoad : Group->Void, ?onProgress : ProgressEvent->Void, ?onError: ErrorEvent->Void ) : Void;
 	function setPath( path : String ) : FBXLoader;
 	function setResourcePath( value : String ) : FBXLoader;
 	function setCrossOrigin( value : String ) : FBXLoader;
--- src/om/three/loader/GLTFLoader.hx
+++ src/om/three/loader/GLTFLoader.hx
@@ -1,15 +1,35 @@
 package om.three.loader;
 
+import js.html.ErrorEvent;
+import js.html.ProgressEvent;
+import three.cameras.Camera;
 import three.loaders.LoadingManager;
+import three.objects.Group;
+import three.scenes.Scene;
+
+typedef GLTFParser = Dynamic;
+
+typedef GLTF = {
+	scene : Scene,
+	scenes : Array<Scene>,
+	cameras : Array<Camera>,
+	animations: Array<Dynamic>, //TODO
+	asset: {
+		generator : String,
+		version : String
+	},
+	parser: GLTFParser,
+	userData: Dynamic
+}
 
 @:native("THREE.GLTFLoader")
 extern class GLTFLoader {
 	var crossOrigin(default,null) : String;
 	function new( ?manager : LoadingManager ) : Void;
-	function load( url : String, ?onLoad : Dynamic->Void, ?onProgress : Dynamic->Void, ?onError : Dynamic->Void ) : Void;
+	function load( url : String, ?onLoad : GLTF->Void, ?onProgress : ProgressEvent->Void, ?onError : ErrorEvent->Void ) : Void;
 	function setCrossOrigin( value : String ) : GLTFLoader;
 	function setPath( path : String ) : GLTFLoader;
 	function setResourcePath( value : String ) : GLTFLoader;
 	function setDRACOLoader( dracoLoader : String ) : GLTFLoader;
-	function parse( data : Dynamic, path : String, ?onLoad : Dynamic->Void, ?onError : Dynamic->Void ) : Void;
+	function parse( data : Dynamic, path : String, ?onLoad : GLTF->Void, ?onError : ErrorEvent->Void ) : Void;
 }
